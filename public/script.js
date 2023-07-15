9033785819049ec9e182cf7b7338af5219bdc66d
Biju Aryal <bijuaryal17@gmail.com>
2023-07-15 22:45:38 +0545

    Collaborative white board

---
+++ public/script.js
@@ -0,0 +1,43 @@
+let canvas = document.getElementById('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var io = io.connect('http://localhost:8080');
+
+let ctx = canvas.getContext('2d');
+
+let x, y;
+
+let mouseDown = false;
+
+window.onmousedown = (e) => {
+    ctx.moveTo(x, y);
+    io.emit('down',{x,y});
+    mouseDown = true;
+}
+window.onmouseup = (e) => {
+    mouseDown = false;
+}
+
+io.on('ondraw', ({ x, y }) => {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+});
+
+io.on('ondown',({x,y})=>
+{
+    ctx.moveTo(x,y);
+})
+
+window.onmousemove = (e) => {
+    x = e.clientX;
+    y = e.clientY;
+
+
+    if (mouseDown) {
+        io.emit('draw', { x, y });
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
+} 
